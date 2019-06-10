b5713bfc8c4421012523788af785f6494abb2703
seanjaw <sjaw94@gmail.com>
2019-06-10 14:20:01 -0700

    connected firebase and create native app

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
+import {f ,auth, database} from './config/config.js';
 export default class App extends React.Component {
+  constructor(props)
+  {
+    super(props);
+    // this.registerUser('testemailaddress@gmail.com', 'fakepassword')
+    // this.state = {
+    //   loggedin: false
+    // };
+
+    // var that =this 
+    auth.signOut()
+    .then(()=>{
+      console.log('logged out...')
+    }).catch((error) =>{
+      console.log('error:',error);
+    })
+    f.auth().onAuthStateChanged(function(user){
+      if (user){
+        console.log('logged in')
+      }else{
+        console.log('logged out')
+      }
+    });
+  }
+  registerUser = (email,password) =>{
+    auth.createUserWithEmailAndPassword(email,password)
+    .then((userObj) => console.log(email,password,userObj))
+    .catch((error) => console.log('error logging in', error));
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight
+        style ={{backgroundColor: 'green'}}
+        >
+        <Text style = {{color:'white'}}>Login With FB</Text>
+        </TouchableHighlight>
       </View>
     );
   }
